6ebc2d5ad8ef901c3613945b3ee8f8b821f5755e
1SnoS13208 <sonvu13208@gmail.com>
2026-01-10 21:46:22 +0700

    Ininital project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.example.thenewsapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.thenewsapp"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx.v1120)
+    implementation(libs.androidx.appcompat.v161)
+    implementation(libs.material.v1100)
+    implementation(libs.androidx.constraintlayout.v214)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
+
+    // Architectural Components
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Room
+    implementation (libs.androidx.room.runtime.v260)
+    ksp (libs.androidx.room.compiler.v284)
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation (libs.androidx.room.ktx.v260)
+
+    // Coroutines
+    implementation (libs.kotlinx.coroutines.core.v171)
+    implementation (libs.kotlinx.coroutines.android.v171)
+
+    // Coroutine Lifecycle Scopes
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.runtime.ktx.v2100)
+
+    // Retrofit
+    implementation (libs.retrofit.v290)
+    implementation (libs.converter.gson.v290)
+    implementation (libs.logging.interceptor.v450)
+
+    // Navigation Components
+    implementation (libs.androidx.navigation.fragment.ktx.v275)
+    implementation (libs.androidx.navigation.ui.ktx.v296)
+
+    // Glide
+    implementation (libs.glide)
+    ksp (libs.compiler.v505)
+}
---
+++ app/src/main/java/com/example/thenewsapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thenewsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.thenewsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
